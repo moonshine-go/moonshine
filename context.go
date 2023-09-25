5fdde688620acb706b1ec9ad8eb91306fe55b332
yamasaki <yamasakipann0218@gmail.com>
2023-09-26 04:33:42 +0900

    fix : context struct

--- context.go
+++ context.go
@@ -1,10 +1,16 @@
 package moonshine
 
-import "net/http"
+import (
+	"context"
+	"net/http"
+	"sync"
+)
 
 type Context struct {
 	request *http.Request
 	writer  http.ResponseWriter
+	mu      sync.RWMutex
+	errors  []error
 }
 
 func (c *Context) Request() *http.Request {
@@ -13,6 +19,19 @@ func (c *Context) Request() *http.Request {
 
 func (c *Context) Writer() http.ResponseWriter {
 	return c.writer
+}
+
+func (c *Context) Context() context.Context {
+	return c.request.Context()
+}
+
+func (c *Context) Error(err error) {
+	if err == nil {
+		panic("err is nil")
+	}
+	c.mu.Lock()
+	c.errors = append(c.errors, err)
+	c.mu.Unlock()
 }
 
 func NewCtx(req http.Request, res http.ResponseWriter) *Context {
