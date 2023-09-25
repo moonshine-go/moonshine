3517b37023e0325770dd6a7a625a2d7e7e62874c
yamasaki <yamasakipann0218@gmail.com>
2023-09-26 00:59:31 +0900

    feat : context struct init

--- context.go
+++ context.go
@@ -1,1 +1,23 @@
 package moonshine
+
+import "net/http"
+
+type Context struct {
+	request *http.Request
+	writer  http.ResponseWriter
+}
+
+func (c *Context) Request() *http.Request {
+	return c.request
+}
+
+func (c *Context) Writer() http.ResponseWriter {
+	return c.writer
+}
+
+func New(req http.Request, res http.ResponseWriter) *Context {
+	return &Context{
+		request: &req,
+		writer:  res,
+	}
+}
